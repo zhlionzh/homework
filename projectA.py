773dc07969faaab3966ffd0e1a2005c7644ffaf0
zhlionzh <49984334+zhlionzh@users.noreply.github.com>
2020-07-31 13:20:46 +0800

    Add files via upload

--- projectA.py
+++ projectA.py
@@ -2,14 +2,15 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
+
 def get_soup(page):
     # 请求URL
-    url = 'http://car.bitauto.com/xuanchegongju/?mid=8'.format(page+1)
+    url = 'http://car.bitauto.com/xuanchegongju/?mid=8&page={}'.format(page+1)
     # 得到页面的内容
-    headers={'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36'}
-    html=requests.get(url,headers=headers,timeout=10)
+    headers = {'user-agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36'}
+    html = requests.get(url, headers=headers, timeout=10)
     content = html.text
-    return (content)
+    return content
 
 def data_analysis(page):
     df = pd.DataFrame(columns=['car_model', 'lowest_price', 'hightest_price', 'picture_url'])
@@ -20,14 +21,17 @@ def data_analysis(page):
         # 找到完整的投诉信息框
         temp = soup.find('div', class_="search-result-list")
         # 创建DataFrame
-
         a_list = temp.find_all('a')
         for a in a_list:
             temp = {}
             temp['car_model'] = a.find_all('p', class_="cx-name text-hover")[0].text
             price = a.find_all('p', class_="cx-price")[0].text.split('-')
-            temp['lowest_price'] = price[0]
-            temp['hightest_price'] = price[1][:-1]
+            if len(price)>1:
+                temp['lowest_price'] = price[0]+'万'
+                temp['hightest_price'] = price[1]
+            else:
+                temp['lowest_price'] = price[0]
+                temp['hightest_price'] = price[0]
             temp['picture_url'] = a.find_all('img')[0]['src']
             df = df.append(temp, ignore_index= True)
     return df
